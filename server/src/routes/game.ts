c117a19c72e49bc6a099e92f21b250730b2949da
abbasnadir <abbasnadir12345@gmail.com>
2026-04-04 18:45:27 +0530

    Added a instance tracking table for players in database, created game/create route to create a lobby

---
+++ server/src/routes/game.ts
@@ -0,0 +1,66 @@
+import type { Request, Response } from "express";
+import type { RouterObject } from "../../types/router.js";
+import { supabase } from "../lib/supabaseClient.js";
+
+/* GET home page. */
+const gameRouter: RouterObject = {
+  path: "/game",
+  functions: [
+    {
+      method: "get",
+      props: "/create",
+      authorization: "required",
+      rateLimit: "strict",
+      keyType: "user",
+      handler: async (req: Request, res: Response) => {
+        const { data: user, error } = await supabase
+          .from("profiles")
+          .select("id")
+          .eq("id", req.user.id)
+          .is("deleted_at", null)
+          .single();
+
+        if (error || !user) {
+          throw new Error("User not found or deleted");
+        }
+
+        const { data: gameData, error: gameError } = await supabase
+          .from("game_round_players")
+          .select(
+            `
+                game_round_id,
+                game_rounds!inner(status)
+            `,
+          )
+          .eq("user_id", req.user.id)
+          .is("left_at", null)
+          .eq("game_rounds.status", "in_progress")
+          .limit(1);
+
+        if (gameError) {
+          throw gameError;
+        }
+
+        if (gameData.length > 0) {
+          throw new Error("You're already part of an active game.");
+        }
+
+        const { data: newGame, error: newGameError } = await supabase
+          .from("game_rounds")
+          .insert({
+            status: "in_progress",
+          })
+          .select("id")
+          .single();
+
+        if (newGameError || !newGame) {
+          throw newGameError || new Error("Failed to create a new game.");
+        }
+
+        res.status(201).json({ gameId: newGame.id });
+      },
+    },
+  ],
+};
+
+export default gameRouter;
